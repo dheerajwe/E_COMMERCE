1c7595a27a0f6ab0eec8e24490dc7b4531e45c82
dheerajwe <dheerajmande@gmail.com>
2025-02-13 01:20:41 +0530

    Captail

--- frontend/src/pages/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder.jsx
@@ -2,7 +2,7 @@ import React, { useContext, useState } from "react";
 import axios from "axios";
 import { toast } from "react-toastify";
 import Title from "../components/Title";
-import CartTotal from "../components/cartTotal";
+import CartTotal from "../components/CartTotal";
 import { assets } from "../assets/assets";
 import { ShopContext } from "../context/ShopContext";
 
